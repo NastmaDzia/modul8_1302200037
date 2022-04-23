ec3dffc130e6184451364853fbf67cac0c4c9d4e
NastmaDzia <100849580+NastmaDzia@users.noreply.github.com>
2022-04-23 17:42:36 +0700

    Menambahkan API Movies

---
+++ modul8_1302200037/Movies.cs
@@ -0,0 +1,12 @@
+﻿namespace modul8_1302200037
+{
+    public class Movies
+    {
+        private string Title { get; set; }
+        private string Director { get; set; }
+        private List<string> Stars { get; set; }
+        private String Description { get; set; }
+    }
+
+     
+    }
